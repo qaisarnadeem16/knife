f8bcf751620b1a01915ec493198ee94021b1f155
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2025-01-11 00:21:59 +0500

    update

--- src/components/layouts/mobile/SelectorMobile.tsx
+++ src/components/layouts/mobile/SelectorMobile.tsx
@@ -185,7 +185,7 @@ const SelectorMobile = () => {
 		} catch (e) { }
 	};
 
-	
+
 	// Initial template selection
 	useEffect(() => {
 		if (templates.length > 0 && !currentTemplate) setTemplate(templates[0].id);
